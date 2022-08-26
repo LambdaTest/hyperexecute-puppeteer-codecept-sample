170c645400abd5a2ac471dcb2b956242c13a9807
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-08-26 22:35:47 +0530

    [Hyperexecute] codecept with puppeteer framework

---
+++ codecept.conf.js
@@ -0,0 +1,50 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+const capabilities = {
+  'browserName': 'Chrome',
+  'browserVersion': 'latest',
+  'LT:Options': {
+    'platform': process.env.HYPEREXECUTE_PLATFORM,
+    'build': 'Puppeteer CodeceptJS HyperExecute Build',
+    'name': 'Puppeteer CodeceptJS HyperExecute Test',
+    'user': process.env.LT_USERNAME,
+    'accessKey': process.env.LT_ACCESS_KEY,
+    'network': true,
+    'video': true,
+    'console': true
+  }
+};
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      chrome: {
+        browserWSEndpoint: `wss://cdp.lambdatest.com/puppeteer?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+      }
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'puppeteer-codecept-sample',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
