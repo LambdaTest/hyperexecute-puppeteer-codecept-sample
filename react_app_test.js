2fe02e6f028f2b424bbf2cc7eae7d8d5c464a246
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-08-30 23:23:11 +0530

    updated test

--- react_app_test.js
+++ react_app_test.js
@@ -3,10 +3,15 @@ Feature('React Apps');
 Scenario('try react app', ({ I }) => {
   I.amOnPage('https://ahfarmer.github.io/calculator/');
   I.click('7');
+  I.wait(10);
   I.seeElement({ react: 't', props: { name: '5' } });
   I.click('button', { react: 't', props: { name: '2' } });
+  I.wait(10);
   I.click('button', { react: 't', props: { name: '+' } });
+  I.wait(10);
   I.click('button', { react: 't', props: { name: '9' } });
+  I.wait(10);
   I.click('button', { react: 't', props: { name: '=' } });
+  I.wait(10);
   I.seeElement({ react: 't', props: { value: '81' } });
 });
--- todo_test.js
+++ todo_test.js
@@ -2,9 +2,14 @@ Feature('ToDo');
 
 Scenario('create todo item', ({ I }) => {
   I.amOnPage('http://todomvc.com/examples/react/');
+  I.wait(10);
   I.dontSeeElement('.todo-count');
+  I.wait(10);
   I.fillField('What needs to be done?', 'Write a guide');
+  I.wait(10);
   I.pressKey('Enter');
+  I.wait(10);
   I.see('Write a guide', '.todo-list');
+  I.wait(10);
   I.see('1 item left', '.todo-count');
 });
