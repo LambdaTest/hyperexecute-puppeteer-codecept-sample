b27a46a778b52bbcc01fc119b4479be0be05891c
Ritik Gagde <ritikg@LT-MBP-124.local>
2024-03-11 20:59:48 +0530

    fixing repos

--- react_app_test.js
+++ react_app_test.js
@@ -1,17 +1,16 @@
 Feature('React Apps');
 
 Scenario('try react app', ({ I }) => {
-  I.amOnPage('https://ahfarmer.github.io/calculator/');
+  I.amOnPage('https://zxcodes.github.io/Calculator/');
   I.click('7');
+  I.wait(5);
+  I.click('2');
+  I.wait(5);
+  I.click('+');
+  I.wait(5);
+  I.click('9');
+  I.wait(5);
+  I.click('=');
   I.wait(10);
-  I.seeElement({ react: 't', props: { name: '5' } });
-  I.click('button', { react: 't', props: { name: '2' } });
-  I.wait(10);
-  I.click('button', { react: 't', props: { name: '+' } });
-  I.wait(10);
-  I.click('button', { react: 't', props: { name: '9' } });
-  I.wait(10);
-  I.click('button', { react: 't', props: { name: '=' } });
-  I.wait(10);
-  I.seeElement({ react: 't', props: { value: '81' } });
+  I.seeElement('#result');
 });
